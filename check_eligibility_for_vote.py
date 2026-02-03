e6242ee4a025832c511d35a2ce8edffd98f4f936
Manish chauhan <nitm.manishchauhan@gmail.com>
2026-02-03 15:40:29 +0530

    Add eligibility check for voting based on age

---
+++ check_eligibility_for_vote.py
@@ -0,0 +1,7 @@
+#CHECK ELIGIBILITY FOR VOTE
+name = input("enter name :")
+age = int(input("enter age :"))
+if age >= 18 :
+  print (name ,"you are eligible for vote")
+else :
+  print (name, " you are a minor now ")
