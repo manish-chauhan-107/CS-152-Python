dcdc798a194a5c6076dc7cfbfc387219e3ee035c
Manish chauhan <nitm.manishchauhan@gmail.com>
2026-02-03 15:46:44 +0530

    Delete check_eligibility_for_vote.py

--- check_eligibility_for_vote.py
+++
@@ -1,7 +0,0 @@
-#CHECK ELIGIBILITY FOR VOTE
-name = input("enter name :")
-age = int(input("enter age :"))
-if age >= 18 :
-  print (name ,"you are eligible for vote")
-else :
-  print (name, " you are a minor now ")
